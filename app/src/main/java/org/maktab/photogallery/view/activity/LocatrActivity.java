8728d5ad5b1d3d0dffbe409d1c0b800da2794a43
ctrlslash <1ace28d67b>
2020-12-05 14:02:13 +0330

    Adding permission to get location. Using google play service to get the fused location.

--- app/src/main/java/org/maktab/photogallery/view/activity/LocatrActivity.java
+++ app/src/main/java/org/maktab/photogallery/view/activity/LocatrActivity.java
@@ -3,13 +3,20 @@ package org.maktab.photogallery.view.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import org.maktab.photogallery.view.fragment.LocatrFragment;
 
 public class LocatrActivity extends SingleFragmentActivity {
+
+    private static final int REQUEST_ERROR = 0;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, LocatrActivity.class);
@@ -19,4 +26,22 @@ public class LocatrActivity extends SingleFragmentActivity {
     public Fragment createFragment() {
         return LocatrFragment.newInstance();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = apiAvailability.getErrorDialog(this, errorCode, REQUEST_ERROR,
+                    new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            finish();
+                        }
+                    });
+            errorDialog.show();
+        }
+    }
+
 }
--- app/src/main/java/org/maktab/photogallery/view/fragment/LocatrFragment.java
+++ app/src/main/java/org/maktab/photogallery/view/fragment/LocatrFragment.java
@@ -1,11 +1,19 @@
 package org.maktab.photogallery.view.fragment;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,12 +21,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
 import org.maktab.photogallery.R;
 import org.maktab.photogallery.databinding.FragmentLocatrBinding;
 
+import java.util.concurrent.TimeUnit;
+
 public class LocatrFragment extends Fragment {
 
+    private static final String TAG = "LocatrFragment";
+    private static final int REQUEST_CODE_PERMISSION_LOCATION = 0;
     private FragmentLocatrBinding mBinding;
+
+    private FusedLocationProviderClient mFusedLocationClient;
 
     public LocatrFragment() {
         // Required empty public constructor
@@ -35,6 +55,8 @@ public class LocatrFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
     }
 
     @Override
@@ -60,10 +82,78 @@ public class LocatrFragment extends Fragment {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_item_location:
-                //TODO: request location permission at runtime
+                if (hasLocationAccess()) {
+                    requestLocation();
+                } else {
+                    //request Location access permission
+                    requestLocationAccessPermission();
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION_LOCATION:
+                if (grantResults == null || grantResults.length == 0)
+                    return;
+
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    requestLocation();
+
+                    return;
+        }
+    }
+
+    private boolean hasLocationAccess() {
+        boolean isFineLocation = ContextCompat.checkSelfPermission(
+                getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        boolean isCoarseLocation = ContextCompat.checkSelfPermission(
+                getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        return isFineLocation && isCoarseLocation;
+    }
+
+    private void requestLocationAccessPermission() {
+        String[] permissions = new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+        requestPermissions(permissions, REQUEST_CODE_PERMISSION_LOCATION);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestLocation() {
+        if (!hasLocationAccess())
+            return;
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setNumUpdates(1);
+        locationRequest.setInterval(0);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Location location = locationResult.getLocations().get(0);
+                Log.d(TAG,
+                        "lat: " + location.getLatitude() + ", lon: " + location.getLongitude());
+            }
+        };
+
+        mFusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
     }
 }
